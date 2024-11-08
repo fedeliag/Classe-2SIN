d15bcbb2574e417999c2b1357f011f5b988ece60
fedeliag <95583643+fedeliag@users.noreply.github.com>
2024-11-08 18:23:36 +0100

    Add files via upload

---
+++ Anni_2Sinf.py
@@ -0,0 +1,6 @@
+anniuser= input("quanti anni hai?")
+print()
+annocorrente= input ("in che anno siamo?")
+annonascita= int(annocorrente) - int(anniuser)
+print()
+print ("Anno di nascita:",annonascita)
